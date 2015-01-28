512c1c58f522bfc96a46a84e49f668b321e80ffc
dnaumenko@smarttech.com <dnaumenko@gmail.com>
2015-01-28 13:32:50 +0200

    added WimmEntity interface

--- WiMM/src/com/naumenko/wimm/dao/WimmDAO.java
+++ WiMM/src/com/naumenko/wimm/dao/WimmDAO.java
@@ -1,5 +1,7 @@
 package com.naumenko.wimm.dao;
 
+import com.naumenko.wimm.dao.entity.WimmEntity;
+
 public interface WimmDAO {
 	
 	public void testAddEntity(WimmEntity entity);
---
+++ WiMM/src/com/naumenko/wimm/dao/entity/WimmEntity.java
@@ -0,0 +1,5 @@
+package com.naumenko.wimm.dao.entity;
+
+public interface WimmEntity {
+
+}
